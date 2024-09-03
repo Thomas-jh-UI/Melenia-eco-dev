5a29f1e494735c13ccfb5104dec63cf29b8b6007
Thomas-jh-UI <129417871+Thomas-jh-UI@users.noreply.github.com>
2024-09-03 20:51:20 +0200

    Create prix-drogues.lua

---
+++ prix-drogues.lua
@@ -0,0 +1,38 @@
+DrugsPrice = {
+        ['weed_whitewidow'] = {
+            min = 15,
+            max = 24,
+        },
+        ['weed_ogkush'] = {
+            min = 15,
+            max = 28,
+        },
+        ['weed_skunk'] = {
+            min = 15,
+            max = 31,
+        },
+        ['weed_amnesia'] = {
+            min = 18,
+            max = 34,
+        },
+        ['weed_purplehaze'] = {
+            min = 18,
+            max = 37,
+        },
+        ['weed_ak47'] = {
+            min = 18,
+            max = 40,
+        },
+        ['crack_baggy'] = {
+            min = 18,
+            max = 34,
+        },
+        ['cokebaggy'] = {
+            min = 18,
+            max = 37,
+        },
+        ['meth'] = {
+            min = 18,
+            max = 40,
+        },
+    },
